325b0fecab2ec97debbaeb6c4c275c0424382791
hamdard345 <58349754+hamdard345@users.noreply.github.com>
2023-12-13 23:52:37 +0000

    index file updated for all task endpoint

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 <?php
- 
+
 // Set the content type to JSON and allow access from any origin
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 
 // If the request method is OPTIONS, exit the script
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {    
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit(0);
 }
 
 /**
  * Define a secret key for the application
  */
-define('SECRET',"4PpgK/?h.Y<2G7lbnM'}t3QGbt&|e");
+define('SECRET', "4PpgK/?h.Y<2G7lbnM'}t3QGbt&|e");
 
 // Include the configuration file which registers the autoloader
 include 'config/config.php';
@@ -28,8 +28,8 @@ $request = new Request();
  * Use a switch statement to match the request path and create the appropriate endpoint object.
  * If the path does not match any cases, create a ClientError object with a 404 error.
  */
-try{
-    switch($request->getPath()) {
+try {
+    switch ($request->getPath()) {
         case '/gettasks/':
         case '/gettasks':
             $endpoint = new GetTasks($db);
@@ -42,10 +42,14 @@ try{
         case '/delete':
             $endpoint = new DeleteTask($db);
             break;
+        case '/alltasks/':
+        case '/alltasks':
+            $endpoint = new AllTasks($db);
+            break;
         default:
             $endpoint = new ClientError("Path not found: " . $path, 404);
     }
-} catch (ClientException $e){
+} catch (ClientException $e) {
     $endpoint = new ClientError($e->getMessage(), $e->getCode());
 }
 
